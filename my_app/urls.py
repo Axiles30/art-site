511bf6be53bb5885696bcdb2a7c7bff88d2a5027
Axiles30 <stroykomp01@gmail.com>
2022-10-10 17:10:59 +0400

    Third commit

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,5 +4,9 @@ from .import views
 
 urlpatterns = [
     path('my_app/', views.start_page, name='start_page'),
+    path('work/', views.work, name='works'),
+    path('shop/', views.shop, name='shop'),
+    path('about/', views.about, name='about'),
+
 
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def start_page(request, *args, **kwargs):
     return render(request, 'my_app/start_page.html', {})
+
+
+def work(request, *args, **kwargs):
+    return render(request, 'works/works.html', {})
+
+
+def shop(request, *args, **kwargs):
+    return render(request, 'shop/shop.html', {})
+
+
+def about(request, *args, **kwargs):
+    return render(request, 'about/about.html', {})
